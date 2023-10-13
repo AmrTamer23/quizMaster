bfa938d54667b3b204e9fe630c0432d7d60d9bb1
Amr Tamer <Amrtamer23mj@gmail.com>
2023-10-13 21:05:33 +0300

    setup the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'cText': '#333333',
+        'navyBlue': '#2b3d50',
+        'secondary': '#e77e23',
+       },
       },
-    },
   },
   plugins: [],
 }
